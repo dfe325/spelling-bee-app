97df38972d25430b41bc8f3f34e72a7a3812897c
Dan Eder <daneder@gmail.com>
2020-07-15 11:34:30 -0400

    saves input as li, does not render on results

--- spelling-bee-app/app.py
+++ spelling-bee-app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template
+     render_template, flash
 from .forms import InputForm
 
 app = Flask(__name__)
@@ -7,10 +7,19 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = "b'*3x0f0xf42x924ow5xdayxc8txacHfxc6qxb16x9efxe9h4xcb4wjxa0Xidxb0ix1dzox8ffdx03hxc98x90yxd2@qxfcgx14"
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+@app.route('/home', methods=['GET', 'POST'])
+
+def input():
+    #form = InputForm(request.form)
+    input_li = []
+    if request.method == "POST":
+        req = request.form
+        input_li=list(req.values())
+        print(input_li)
+        return redirect('results')
     return render_template('home.html')
 
 @app.route('/results', methods=['GET'])
 def results():
-    form = InputForm()
-    return render_template('results.html', title='Input', form=form)
+
+    return render_template('results.html')
--- spelling-bee-app/forms.py
+++ spelling-bee-app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class InputForm(FlaskForm):
-    letter = StringField('Letter', validators=[DataRequired(), Length(min=2, max=20)])
-    submit = SubmitField('Click Here')
+    letter = StringField('Letter', validators=[DataRequired(), Length(max=10, message=('Too many letters.'))])
+    submit = SubmitField('Submit')
